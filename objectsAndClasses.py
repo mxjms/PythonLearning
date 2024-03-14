76c08bdbff45d4b5080aeb321121a5bd561a8aab
Max James <max.james@fdmgroup.com>
2024-03-14 15:21:39 -0400

    Classes, constructors and Methods

--- objectsAndClasses.py
+++ objectsAndClasses.py
@@ -1,5 +1,27 @@
+#CLASSES
+class Dog:
+
+    # Constructor: include two variables to pass in when creating a dog object
+        def __init__(self,name,age):
+              self.name = name
+              self.age = age
+
+    # Method
+        def bark(self):     # self as argument of the method will point to the current object instance & must be specified when defining a method
+            print("Woof!")  # When creating a method in a class always start with self
+
+    #######################
+
+
 # Objects
 # Attributes and Methods
+        
+roger = Dog("Roger", 6) # will assign name and age
+
+print(type(roger))      # will show roger is a dog
+print(roger.age)        # will show age
+
+roger.bark()     # will return "woof!"   --- allows object to call method
 
 age = 8
 
@@ -13,3 +35,14 @@ items = [1, 2]
 
 items.append(3)
 items.pop()
+
+    #######################
+
+# INHERITANCE
+class Pitbull(Dog):
+        def grow(self):
+            print("I'm aging!")
+
+taz = Pitbull("Taz", 2)
+
+taz.bark()       # will return 'woof!' --- Pitbull class has inherited methods from Dog class
